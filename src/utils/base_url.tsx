0b80b2d6e9d420b030d873ca96e1b6f552151e39
amresh012 <amreshmishra67099@gmail.com>
2025-05-17 17:36:59 +0530

    fix: update base_url to include '/api/' in the production URL

--- src/utils/base_url.tsx
+++ src/utils/base_url.tsx
@@ -1,2 +1,2 @@
 // export const base_url  = "http://localhost:5000/api/"
-export const base_url  = "https://livestreamingappbackend-production-e4f8.up.railway.app/"
+export const base_url  = "https://livestreamingappbackend-production-e4f8.up.railway.app/api/"
